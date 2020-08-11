3160d5be929eb41d3c6bd2014d3078dda692daad
gminteer <gminteer@gmail.com>
2020-08-10 20:43:52 -0400

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2017: true,
+  },
+  globals: {
+    __basedir: 'readonly',
+  },
+  plugins: ['prettier', 'promise', 'import', 'node', 'compat'],
+  extends: ['eslint:recommended', 'google', 'prettier'],
+  rules: {
+    'prettier/prettier': ['warn'],
+    'no-template-curly-in-string': ['warn'],
+    'prefer-template': ['warn'],
+    'require-jsdoc': ['off'],
+    'no-debugger': ['warn'],
+    'vars-on-top': ['warn'],
+    'brace-style': ['error', '1tbs', {allowSingleLine: true}],
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'multi-or-nest', 'consistent'],
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+};
