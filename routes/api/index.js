7eee3bcfb5bf65df02f09ba97d3edb0c03d60d32
gminteer <gminteer@gmail.com>
2020-08-11 03:47:02 -0400

    basic routes look like they work

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,31 @@
+const path = require('path');
 const router = require('express').Router();
+const notes = require(path.join(__basedir, 'lib/notes'));
 
-router.get('/notes', (req, res) => {
-  // TODO
+router.get('/notes', async (req, res) => {
+  res.json(await notes.read());
+});
+router.get('/notes/:id', async (req, res) => {
+  const result = await notes.read(req.params.id);
+  if (result.length > 0) res.json(result);
+  else res.sendStatus(404);
 });
 
-router.post('/notes', (req, res) => {
-  // TODO
+router.post('/notes', async (req, res) => {
+  const note = await notes.create(req.body);
+  if (note) res.json(note);
+  else res.sendStatus(400);
 });
 
+router.post('/notes/:id', async (req, res) => {
+  const ok = await notes.update(req.body);
+  if (ok) res.status(200).send(req.body);
+  else res.sendStatus(400);
+});
+
+router.delete('/notes/:id', async (req, res) => {
+  const ok = await notes.drop(req.params.id);
+  if (ok) res.sendStatus(204);
+  else res.sendStatus(400);
+});
 module.exports = router;
