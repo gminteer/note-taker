5f94402867b365f643c91e572e5535af9a1de12d
gminteer <gminteer@gmail.com>
2020-08-14 00:00:49 -0400

    better linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 const devDependencies = Object.keys(require('./package.json').devDependencies) || {};
-
+const basePlugins = ['prettier', 'promise', 'import'];
+const baseExtends = [
+  'eslint:recommended',
+  'plugin:promise/recommended',
+  'plugin:import/errors',
+  'plugin:import/warnings',
+  'google',
+  'prettier',
+];
 module.exports = {
   env: {
     node: true,
-    browser: true,
     es2017: true,
-    'jest/globals': true,
   },
-  plugins: ['prettier', 'promise', 'import', 'node', 'compat', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:promise/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:node/recommended',
-    'google',
-    'prettier',
-  ],
+  plugins: basePlugins.concat(['node', 'security']),
+  extends: baseExtends.concat(['plugin:node/recommended', 'plugin:security/recommended']),
   rules: {
     'prettier/prettier': ['warn'],
     'no-template-curly-in-string': ['error'],
@@ -32,8 +30,28 @@ module.exports = {
   overrides: [
     {
       files: ['__tests__/**/*', '__mocks__/**/*', 'test/**/*'],
+      env: {
+        'jest/globals': true,
+      },
+      plugins: basePlugins.concat(['node', 'jest']),
+      extends: baseExtends.concat(['plugin:node/recommended', 'plugin:jest/recommended', 'plugin:jest/style']),
       rules: {
         'node/no-unpublished-require': ['error', {allowModules: devDependencies}],
+      },
+    },
+    {
+      files: ['public/**/*.js', 'static/**/*.js'],
+      plugins: basePlugins.concat(['compat', 'jquery']),
+      env: {
+        node: false,
+        browser: true,
+        jquery: true,
+      },
+      extends: baseExtends.concat(['plugin:compat/recommended']),
+      rules: {
+        'no-var': ['off'],
+        'vars-on-top': ['off'],
+        'prettier/prettier': ['off'],
       },
     },
   ],
